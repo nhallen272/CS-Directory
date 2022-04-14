da6f4d6d2955ac10d071c2114246f2c3918e7128
Nathan Allen <nh.allen272@gmail.com>
2022-04-14 13:50:49 -0400

    fixed scraper

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 #Scrape 
 # Directory URL = https://odu.edu/compsci/directory
+from asyncio.windows_events import NULL
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FacultyDisplay.settings")
 import django
@@ -10,6 +11,8 @@ import requests
 from bs4 import BeautifulSoup
 from display.models import FacultyModel
 
+CS_BASE_URL = "https://www.cs.odu.edu/"
+
 class Faculty:
     def __init__(self, name, title, picURL, address, phone, email):
         self.name = name
@@ -18,6 +21,9 @@ class Faculty:
         self.address = address
         self.phone = phone
         self.email = email
+        usernames = email.split("@")
+        self.username = "~" + usernames[0]
+        self.website = CS_BASE_URL + self.username 
     
     def print(self):
         print("Name {}".format(self.name))
@@ -26,6 +32,8 @@ class Faculty:
         print("Address {}".format(self.address))
         print("Phone {}".format(self.phone))
         print("Email {}".format(self.email))
+        print("Username {}".format(self.username))
+        print("Website {}".format(self.website))
         print(" ")
 
     def __eq__(self, other):
@@ -71,11 +79,24 @@ def main():
         FacList.append(f)
 
 
+#    heading = models.CharField(max_length=120, default=' ', null=True) # status
+#    website = models.URLField(null=True)                               # link to their .cs.odu.edu website
+#    research = models.ManyToManyField('ResearchCategory')              # research types
+#    bio = models.TextField(default=' ', null=True)                     # personal biography, research, etc.
+#    edited = models.BooleanField(default=False, null=True)
+
+    # websites https://www.cs.odu.edu/~tkennedy
     for fac in FacList:
         fac.print()
+        
         # check if already in DB
-        facAdd = FacultyModel(name=fac.name, )
-        if ( )
+        if FacultyModel.objects.filter(name=fac.name).exists():
+            print("Already Exists")
+        else:
+            facAdd = FacultyModel(title=fac.title, email=fac.email, phone=fac.phone, pic=fac.pic, name=fac.name, website=fac.website)
+            facAdd.save()
+            
+
 
 
     
