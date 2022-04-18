0e12253feaccc34d0819b5ddae02ce9e2bfb854e
Nathan Allen <nh.allen272@gmail.com>
2022-04-18 18:34:53 -0400

    updated to do

--- FacultyDisplay/settings.py
+++ FacultyDisplay/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = join(BASE_DIR, 'staticfiles')
 
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [
     join(BASE_DIR, "static"),
     #join(BASE_DIR, "display/static"),
